d455c4532da5936fecf73baa1ed830de5ed8118a
MaxWu <2386140382@qq.com>
2018-06-21 13:54:15 +0800

    upload h6

--- homework6/grp_server.c
+++ homework6/grp_server.c
@@ -19,6 +19,10 @@
 #include <net/if.h>
 #include <linux/if_packet.h>
 #include <netinet/if_ether.h>
+#include <sys/ioctl.h>
+#include <linux/sockios.h>
+
+#define DB_DEBUG 1
 
 struct DATA_PROTO
 {
@@ -30,8 +34,11 @@ struct DATA_PROTO
 
 int main (int argc, char **argv)
 {
-	int sockfd;
+	int tempSocket;
   struct DATA_PROTO sendInfo;
+  struct ifreq buffer;
+  unsigned char pLocalMAC[6];
+  char *strInterfaceName = "wlp3s0";
 
   // sendInfo.dwGroupID = (int *)&argv[1];
   // strcpy(sendInfo.dwGroupID, argv[1]);
@@ -49,15 +56,31 @@ int main (int argc, char **argv)
 	}
 	printf("(int *s)argv[1]: %ls\n", (int *)argv[1]);
 
-  sockfd = socket(PF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
-  // sockfd = socket(PF_PACKET, SOCK_RAW, 0);
-  printf("sockfd: %d\n", sockfd);
-  if (sockfd == -1)
+  // tempSocket = socket(PF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
+  tempSocket = socket(PF_PACKET, SOCK_RAW, 0);
+  printf("tempSocket: %d\n", tempSocket);
+  if (tempSocket == -1)
   {
     printf("create socket error\n");
     return -1;
   }
-  
+
+  memset(&buffer, 0x00, sizeof(buffer));
+  strcpy(buffer.ifr_name, strInterfaceName);
+  if (ioctl(tempSocket, SIOCGIFHWADDR, &buffer) == -1)
+  {
+    printf("ERROR : Can NOT get the local mac address !!!\n");
+    return -2;
+  }
+  else
+  {
+    memcpy((void *)pLocalMAC, (void *)buffer.ifr_hwaddr.sa_data, 6);
+    if (DB_DEBUG)
+    {
+      printf("INFO : LocalMAC = %02X:%02X:%02X:%02X:%02X:%02X\n", pLocalMAC[0], pLocalMAC[1], pLocalMAC[2], pLocalMAC[3], pLocalMAC[4], pLocalMAC[5]);
+    }
+  }
+
   printf("so...,the next~\n");
 
 	return 0;
